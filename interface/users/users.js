3541c91cab6fb65e88352b5f065e86c870031765
yexuan <yexuan@yexuan.site>
2018-09-02 01:12:12 +0800

    fix some bugs modified some logic...

--- interface/users/users.js
+++ interface/users/users.js
@@ -1,4 +1,5 @@
-const promise=require('../../Promise/promise')
+const promise=require('../../Promise/promise');
+const time=require('../../time/time');
 module.exports={
     //查询用户 by openid
     selectOneByOpenid:function(req,res){
@@ -29,15 +30,20 @@ module.exports={
     insertUsers:function(req,res){
         function sel(req,res){         
             var openid=req.query.openid; 
-            var name=req.query.name;
+            var name=req.query.name;            
+            var real_name=req.query.real_name;
             var school=req.query.school;
+            var grade=req.query.grade;
+            var e_mail=req.query.e_mail;
+            var gander=req.query.gander;
             var icon_url=req.query.icon_url;
             var age=req.query.age;
             var love=req.query.love;
             var birthday=req.query.birthday;
             var city=req.query.city;
             var sign=req.query.sign;  
-            var sql=`insert into users values("${openid}","${name}","${school}","${icon_url}","${age}","${love}","${birthday}","${city}","${sign}");`;
+            var sql=`insert into users values("${openid}","${name}","${real_name}","${gander}","${school}","${grade}",
+                    "${e_mail}","${time.getTime()}","${icon_url}","${age}","${love}","${birthday}","${city}","${sign}");`;
             console.log(sql)
             fun(sql);
         }
